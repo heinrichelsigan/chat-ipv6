ace7838e545a74b09fa7965a885a5ee7836e3acd
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2025-02-09 16:59:53 +0100

    2025-02-09 java: added determine to internet connected network interface, so that this network ip address is selected by default

--- java/cqrxs.eu/fw/net/NetworkAddresses.java
+++ java/cqrxs.eu/fw/net/NetworkAddresses.java
@@ -9,7 +9,36 @@ public class NetworkAddresses {
         Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
         for (NetworkInterface netint : Collections.list(nets))
             displayInterfaceInformation(netint);
+		
+		getConnectedAddress();
     }
+	
+	public static void getConnectedAddress() {
+		Socket s;
+		try {
+			s = new Socket("18.100.254.167", 80);
+			System.out.println(s.getLocalAddress().getHostAddress());
+			s.close();
+			return;
+		} catch (Exception ex) { 
+		}
+		try {
+			s = new Socket("15.188.56.225", 80);
+			System.out.println(s.getLocalAddress().getHostAddress());
+			s.close();
+			return;
+		} catch (Exception ex) { 
+		}
+		try {
+			s = new Socket("35.168.3.151", 80);
+			System.out.println(s.getLocalAddress().getHostAddress());
+			s.close();
+			return;			
+		} catch (Exception ex) { 
+		}
+		
+		return;
+	}
 
     static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
         out.printf("Display name: %s\n", netint.getDisplayName());
