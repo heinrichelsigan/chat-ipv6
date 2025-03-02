75e0135a04d5788c2060491f297d697d39bc09a5
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2025-03-02 10:56:50 +0100

    Create ICqrMessageable.cs

---
+++ C#/Area23.At.Framework/Area23.At.Framework.Library/CqrXs/CqrMsg/ICqrMessageable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area23.At.Framework.Core.CqrXs.CqrMsg
+{
+    public interface ICqrMessagable
+    {
+        public MsgEnum MsgType { get; }
+        public string Hash { get; }
+        public string Message { get; }
+        public string RawMessage { get; }
+
+        public string ToJson();
+        public T? FromJson<T>(string jsonText);
+        public string ToXml();
+        public T? FromXml<T>(string xmlText);
+      
+    }
+}
