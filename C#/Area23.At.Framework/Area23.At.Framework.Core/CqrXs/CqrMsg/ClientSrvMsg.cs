08a84b5e04e0800553b50118d1bd94d2fa32988d
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2025-03-02 11:18:52 +0100

    Update ClientSrvMsg.cs

--- C#/Area23.At.Framework/Area23.At.Framework.Core/CqrXs/CqrMsg/ClientSrvMsg.cs
+++ C#/Area23.At.Framework/Area23.At.Framework.Core/CqrXs/CqrMsg/ClientSrvMsg.cs
@@ -5,6 +5,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace Area23.At.Framework.Library.CqrXs.CqrMsg
+{
+
+    public class ClientSrvMsg<TS, TC> : MsgContent, ICqrMessagable
+        where TS : class
+        where TC : class
+    {
+
+        public FullSrvMsg<TS>? ServerMsg { get; set; }
+        public FullSrvMsg<TC>? ClientMsg { get; set; }
+
+        string ServerMsgString { get; set; }
+
+        string ClientMsgString { get; set; }
+        
+        public ClientSrvMsg()
+        {
+            ServerMsgString = string.Empty;
+            ClientMsgString = string.Empty;
+        }
+
+        public ClientSrvMsg(FullSrvMsg<TS> serverMsg, FullSrvMsg<TC> clientMsg, string serverMsgString, string clientMsgString)
+        {
+            ServerMsg = serverMsg;
+            ClientMsg = clientMsg;
+            ServerMsgString = serverMsgString;
+            ClientMsgString = clientMsgString;
+        }
+
+        public ClientSrvMsg(string serverMsgString, string clientMsgString)
+        {
+            ServerMsgString = serverMsgString;
+            ClientMsgString = clientMsgString;
+        }
+
+        public override string ToJson() => JsonConvert.SerializeObject(this);
+
+        public virtual T? FromJson<T>(string jsonText)
+        {
+            T? t = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(jsonText);
+            if (t != null && t is ClientSrvMsg<TS, TC> csrvmsg)
+            {
+                this._hash = csrvmsg._hash;
+                this._message = csrvmsg._message;
+                this._rawMessage = csrvmsg._rawMessage;
+                this.ClientMsgString = csrvmsg.ClientMsgString;
+                this.ServerMsgString = csrvmsg.ServerMsgString;
+                this.ServerMsg = csrvmsg.ServerMsg;
+                this.ClientMsg = csrvmsg.ClientMsg;
+            }
+            return t;
+        }
+
+        public string ToXml()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? FromXml<T>(string xmlText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
+/*
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Area23.At.Framework.Core.CqrXs.CqrMsg
 {
 
@@ -71,3 +145,4 @@ namespace Area23.At.Framework.Core.CqrXs.CqrMsg
 
 
 }
+*/
