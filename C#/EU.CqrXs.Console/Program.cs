48ffe3794a223f3c974dfcdda89299e0ba8ff415
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2025-03-13 03:18:45 +0100

    2025-03-13 added tesconsole app for zipping, en/de-crypting / en-/de-codeing

--- C#/EU.CqrXs.Console/Program.cs
+++ C#/EU.CqrXs.Console/Program.cs
@@ -1,13 +1,21 @@
 ﻿using Area23.At.Framework.Core.Zfx;
-using System.Reflection;
+using System.Windows;
 
 
 namespace EU.CqrXs.Console
 {
     internal class Program
     {
+
+        static readonly string? progName = System.Environment.ProcessPath;
+        //progName = System.AppDomain.CurrentDomain.FriendlyName;
+        //progName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+        //progName = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        //progName = System.Environment.GetCommandLineArgs()[0];
+        //progName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+
         static void Main(string[] args)
-        {
+        {            
             if (args.Length <= 1)
             {
                 Usage();
@@ -33,10 +41,10 @@ namespace EU.CqrXs.Console
 
         static void Usage()
         {
-            System.Console.Out.WriteLine($"Usage:\t{Path.GetFileName(Assembly.GetExecutingAssembly().FullName)} [-gzip|-bzip|-gunzip|-bunzip] filename");
-            System.Console.Out.WriteLine($"      \t{Path.GetFileName(Assembly.GetExecutingAssembly().FullName)} [-encrypt|-decrypt] -key=secret_key filename");
-            System.Console.Out.WriteLine($"      \t{Path.GetFileName(Assembly.GetExecutingAssembly().FullName)} [-encode|-decode] --encoding=[hex16|hex32|base32|base64|uu] filename");
-            System.Console.Out.WriteLine($"      \t{Path.GetFileName(Assembly.GetExecutingAssembly().FullName)} and many other things in future....");
+            System.Console.Out.WriteLine($"Usage:\t{Path.GetFileName(progName)} [-gzip|-bzip|-gunzip|-bunzip] filename");
+            System.Console.Out.WriteLine($"      \t{Path.GetFileName(progName)} [-encrypt|-decrypt] -key=secret_key filename");
+            System.Console.Out.WriteLine($"      \t{Path.GetFileName(progName)} [-encode|-decode] --encoding=[hex16|hex32|base32|base64|uu] filename");
+            System.Console.Out.WriteLine($"      \t{Path.GetFileName(progName)} and many other things in future....");
             System.Environment.Exit(0);
         }
 
