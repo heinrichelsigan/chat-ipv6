acf32cbee054e8914e4c572e8260ea5444a2d9ed
Heinrich Elsigan <heinrich.elsigan@gmaiil.com>
2025-04-12 02:42:50 +0200

    2025-04-12 RedIs changed again

--- C#/Area23.At.Framework/Area23.At.Framework.Library/Cache/RedIs.cs
+++ C#/Area23.At.Framework/Area23.At.Framework.Library/Cache/RedIs.cs
@@ -1,5 +1,6 @@
 ﻿using Area23.At.Framework.Library.Static;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,17 @@ namespace Area23.At.Framework.Library.Cache
         private static HashSet<string> _allKeys = new HashSet<string>();
         public static string[] AllKeys { get => _allKeys.ToArray(); }
 
+        public static string EndPoint
+        {
+            get
+            {
+                _instance.Value.endpoint = Constants.VALKEY_CACHE_HOST_PORT; // "cqrcachecqrxseu-53g0xw.serverless.eus2.cache.amazonaws.com:6379";                
+                if (ConfigurationManager.AppSettings != null && ConfigurationManager.AppSettings[Constants.VALKEY_CACHE_HOST_PORT_KEY] != null)
+                    _instance.Value.endpoint = (string)ConfigurationManager.AppSettings[Constants.VALKEY_CACHE_HOST_PORT_KEY];
+                return _instance.Value.endpoint;
+            }
+        }
+
         public static StackExchange.Redis.IDatabase Db
         {
             get
@@ -45,7 +57,7 @@ namespace Area23.At.Framework.Library.Cache
                     if (_instance.Value.options == null)
                         _instance.Value.options = new ConfigurationOptions
                         {
-                            EndPoints = { _instance.Value.endpoint },
+                            EndPoints = { EndPoint },
                             Ssl = true
                         };
                     _instance.Value.connMux = ConnectionMultiplexer.Connect(_instance.Value.options);
@@ -108,7 +120,8 @@ namespace Area23.At.Framework.Library.Cache
             if (!_allKeys.Contains(redIsKey))
             {
                 _allKeys.Add(redIsKey);
-                SetKey<string[]>("AllKeys", AllKeys);
+                string jsonVal = JsonConvert.SerializeObject(AllKeys);
+                Db.StringSet("AllKeys", jsonVal, null, false, When.Always, CommandFlags.None);
             }
 
         }
@@ -139,8 +152,7 @@ namespace Area23.At.Framework.Library.Cache
         /// <returns></returns>
         public T GetKey<T>(string redIsKey, CommandFlags flags = CommandFlags.None)
         {
-            string jsonVal = GetString(redIsKey, flags);
-
+            string jsonVal = Db.StringGet(redIsKey, flags); 
             var tValue = JsonConvert.DeserializeObject<T>(jsonVal);
 
             return tValue;
@@ -157,7 +169,8 @@ namespace Area23.At.Framework.Library.Cache
             if (!_allKeys.Contains(redIsKey))
             {
                 _allKeys.Remove(redIsKey);
-                SetKey<string[]>("AllKeys", AllKeys);
+                string jsonVal = JsonConvert.SerializeObject(AllKeys);
+                Db.StringSet("AllKeys", jsonVal, null, false, When.Always, flags);
             }
         }
 
