00565e09c77a0c50176abcf19f015e140744a69f
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2025-08-01 02:41:28 +0200

    2025-08-01 added java uuencode and base64 encode / decode classes and fixed jmake.sh

---
+++ java/eu/cqrxs/fw/crypt/endecoding/EnDeCoder.java
@@ -0,0 +1,9 @@
+package cqrxs.eu.fw.crypt.endecoding;
+
+public abstract class EnDeCoder {
+
+    public abstract String encode(String inString);
+    
+    public abstract String decode(String inString);
+
+}
--- java/eu/cqrxs/fw/crypt/endecoding/UuEnCoder.java
+++ java/eu/cqrxs/fw/crypt/endecoding/UuEnCoder.java
@@ -1,21 +1,21 @@
-package cqrxs.eu.fw.crypt.endecoding;
+package cqrxs.eu.fw.crypt.endecoding; 
 
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Base64Encoder extends Encoder {
+public class UuEnCoder extends Encoder {
     @Override
     Map<Integer, String> getEncodingTable() {
-        String encodingMatrixRaw = "A B C D E F G H " +
-                "I J K L M N O P " +
-                "Q R S T U V W X " +
-                "Y Z a b c d e f " +
-                "g h i j k l m n " +
-                "o p q r s t u v " +
-                "w x y z 0 1 2 3 " +
-                "4 5 6 7 8 9 + /";
+        String encodingMatrixRaw = "‘ ! ” # $ % & ’ " +
+                "( ) * + , - . / " +
+                "0 1 2 3 4 5 6 7 " +
+                "8 9 : ; < = > ? " +
+                "@ A B C D E F G " +
+                "H I J K L M N O " +
+                "P Q R S T U V W " +	
+                "X Y Z [ \\ ] ˆ _ ";
         var encodingMatrix = Arrays.asList(encodingMatrixRaw.split(" "));
         var n = encodingMatrix.size();
         return IntStream.range(0, n).boxed().collect(Collectors.toMap(i -> i, encodingMatrix::get));
@@ -23,10 +23,10 @@ public class Base64Encoder extends Encoder {
 
     @Override
     String getEmptyDataSymbol() {
-        return "=";
+        return "`";
     }
 
     public static void main(String[] args) {
-        System.out.println("KQ: " + new Base64Encoder().encode("Thi"));
+        System.out.println("KQ: " + new UuEncoder().encode("Thi"));
     }
 }
